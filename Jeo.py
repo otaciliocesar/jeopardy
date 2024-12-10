ed483ca26b7915325af12f2af51d7ab1d789947e
otaciliocesar <otaciliocesarsantos@gmail.com>
2024-12-10 12:37:02 -0300

    Nova alteração

--- Jeo.py
+++ Jeo.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 
 jeopardy_data = pd.read_csv("jeopardy.csv")
@@ -8,4 +9,40 @@ jeopardy_data = jeopardy_data.rename(columns = {" Air Date": "Air Date", " Round
 print(jeopardy_data.columns)
 # %%
 print(jeopardy_data["Question"])
+# %%
+# Filtering a dataset by a list of words
+def filter_data(data, words):
+  # Lowercases all words in the list of words as well as the questions. Returns true if all of the words in the list appear in the question.
+  filter = lambda x: all(word in x for word in words)
+  # Applies the lambda function to the Question column and returns the rows where the function returned True
+  return data.loc[data["Question"].apply(filter)]
+
+# Testing the filter function
+filtered = filter_data(jeopardy_data, ["King", "England"])
+print(filtered["Question"])
+# %%
+# Filtering a dataset by a list of words
+def filter_data(data, words):
+  # Lowercases all words in the list of words as well as the questions. Returns true if all of the words in the list appear in the question.
+  filter = lambda x: all(word.lower() in x.lower() for word in words)
+  # Applies the lambda function to the Question column and returns the rows where the function returned True
+  return data.loc[data["Question"].apply(filter)]
+
+# Testing the filter function
+filtered = filter_data(jeopardy_data, ["King", "England"])
+print(filtered["Question"])
+# %%
+# Adding a new column. If the value of the float column is not "None", then we cut off the first character (which is a dollar sign), and replace all commas with nothing, and then cast that value to a float. If the answer was "None", then we just enter a 0.
+jeopardy_data["Float Value"] = jeopardy_data["Value"].apply(lambda x: float(x[1:].replace(',','')) if x != "no value" else 0)
+
+# Filtering the dataset and finding the average value of those questions
+filtered = filter_data(jeopardy_data, ["King"])
+print(filtered["Float Value"].mean())
+# %%
+# A function to find the unique answers of a set of data
+def get_answer_counts(data):
+    return data["Answer"].value_counts()
+
+# Testing the answer count function
+print(get_answer_counts(filtered))
 # %%
